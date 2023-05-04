43c9fc97934451f162d4c8a4dda8781c9866e0b0
Lenix <yyb8@vip.qq.com>
2023-05-04 11:43:19 +0800

    Update App.php

--- App.php
+++ App.php
@@ -2,27 +2,12 @@
 
 include 'vendor/autoload.php';
 (new DotEnv());
+(new ErrorHandel());
 $REQUEST= empty($_REQUEST) ?    file_get_contents("php://input"): $_REQUEST;
 
-try {
-    $router = new Router();
-    foreach (config('route') as $key => $value) {
+$router = new Router();
+foreach (config('route') as $key => $value) {
         $router->addRoute($value[0][0], $value[1], $value[2]);
-    }
-
-    $router->doRouting();
-} catch (Throwable $e) {
-    
-    $errorinfo  =$e->getMessage();
-
-    if ($errorinfo == '404 Not Found') {
-        http_response_code(404);
-        include '404.html';
-    }else{
-
-        http_response_code(500);
-        include '500.html';
-    }
-     
-   
 }
+
+$router->doRouting();
