aebfc86feb5eaae9c3a5335ced845d6d4af3903a
Lenix <yyb8@vip.qq.com>
2023-05-13 12:59:39 +0800

    Create cache.php

---
+++ config/cache.php
@@ -0,0 +1,105 @@
+<?php
+
+// use Illuminate\Support\Str;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Cache Store
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default cache connection that gets used while
+    | using this caching library. This connection is used when another is
+    | not explicitly specified when executing a given caching function.
+    |
+    */
+
+    /*
+    $cacheOneConfig=[
+    'type' => 'redis',
+    'cacheserver' => '127.0.0.1',
+    'schema' => '',
+    'port' => '6379',
+    'user' => NULL,
+    'password' => NULL,
+];
+    */
+
+    'default' => env('CACHE_DRIVER', 'auto'),  //auto 
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Stores
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define all of the cache "stores" for your application as
+    | well as their drivers. You may even define multiple stores for the
+    | same cache driver to group types of items stored in your caches.
+    |
+    | Supported drivers: "apc", "array", "database", "file",
+    |         "memcached", "redis", "dynamodb", "octane", "null"
+    |
+    */
+
+    'stores' => [
+
+        'apcu' => [
+            'driver' => 'apcu',
+        ],
+
+        'array__notavailable' => [
+            'driver' => 'array',
+            'serialize' => false,
+        ],
+
+        'database__notavailable' => [
+            'driver' => 'database',
+            'table' => 'cache',
+            'connection' => null,
+            'lock_connection' => null,
+        ],
+
+        'file__notavailable' => [
+            'driver' => 'file',
+            //'path' => storage_path('framework/cache/data'),
+        ],
+
+        'memcache' => [
+            'driver' => 'memcache',
+            'host'=>'127.0.0.1',
+            'schema' => '',
+            'port'=>'11211',
+            'user' => NULL,
+            'password' => NULL,
+        ],
+
+        'redis' => [
+            'driver' => 'redis',
+            'host'=>'127.0.0.1',
+            'schema' => '',
+            'port'=>6379,
+         //   'user' => NULL,
+          //  'password' => NULL,
+        ],
+
+       
+
+       
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Key Prefix
+    |--------------------------------------------------------------------------
+    |
+    | When utilizing the APC, database, memcached, Redis, or DynamoDB cache
+    | stores there might be other applications using the same cache. For
+    | that reason, you may prefix every cache key to avoid collisions.
+    |
+    */
+
+   // 'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache_'),
+
+];
