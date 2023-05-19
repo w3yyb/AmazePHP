fd6a53cf1a38b602550780bd91e393d1b045f90e
lenix <lenix@localhost.localdomain>
2023-05-19 11:08:58 +0800

    update readme

--- App.php
+++ App.php
@@ -1,4 +1,5 @@
 <?php
+
 define('BASE_PATH', __DIR__);
 include 'lib/DotEnv.php';
 include 'lib/ErrorHandel.php';
@@ -6,27 +7,26 @@ include 'lib/ErrorHandel.php';
 (new ErrorHandel());
 
 include 'vendor/autoload.php';
-$REQUEST= empty($_REQUEST) ?    file_get_contents("php://input"): $_REQUEST;
 
 $reqMet = $_SERVER['REQUEST_METHOD'];
 
-if (config('session.enable')){
-        getSession();
-        cookie('XSRF-TOKEN', session()->token(),  config('session.lifetime'),config('session.path'), config('session.domain'), config('session.secure'), false, config('session.same_site'));
+if (config('session.enable')) {
+    getSession();
+    cookie('XSRF-TOKEN', session()->token(), config('session.lifetime'), config('session.path'), config('session.domain'), config('session.secure'), false, config('session.same_site'));
 }
 
 
 if (config('session.enable') && $reqMet=='POST'  || $reqMet=='PUT' || $reqMet=='DELETE' || $reqMet=='PATCH') {
-        VerifyCsrfToken::getInstance();
+    VerifyCsrfToken::getInstance();
 }
 
 $router = Router::getInstance();
 foreach (config('route') as $key => $value) {
-        $router->addRoute($value[0][0], $value[1], $value[2], $value[3] ?? null);
+    $router->addRoute($value[0][0], $value[1], $value[2], $value[3] ?? null);
 }
 
 $response =$router->doRouting();
 
 if ($response !== null) {
-        echo $response;
+    echo $response;
 }
