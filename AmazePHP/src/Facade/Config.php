e2ea8f96b5d3d4068e943eb45a6c7a8bac2753e4
w3yyb <yyb8@vip.qq.com>
2023-06-18 01:00:23 +0800

    wip

--- AmazePHP/src/Facade/Config.php
+++ AmazePHP/src/Facade/Config.php
@@ -12,7 +12,7 @@ use AmazePHP\Facade;
 class Config extends Facade
 {
     /**
-     * 获取当前Facade对应类名（或者已经绑定的容器对象标识）
+     *
      * @access protected
      * @return string
      */
--- AmazePHP/src/Facade/UrlGenerator.php
+++ AmazePHP/src/Facade/UrlGenerator.php
@@ -11,7 +11,7 @@ use AmazePHP\Facade;
 class UrlGenerator extends Facade
 {
     /**
-     * 获取当前Facade对应类名（或者已经绑定的容器对象标识）
+     *
      * @access protected
      * @return string
      */
