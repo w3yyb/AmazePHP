9421b7e9e0cf6e705f2be27d8f376a5bb487e0cc
w3yyb <yyb8@vip.qq.com>
2023-06-20 18:03:12 +0800

    update

--- config/middleware.php
+++ config/middleware.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-    // App\Middleware\aMiddleware::class,
-    // App\Middleware\bMiddleware::class,
     AmazePHP\Middleware\Cors::class,
-   AmazePHP\Middleware\StartSession::class,
-
+    AmazePHP\Middleware\StartSession::class,
     AmazePHP\Middleware\VerifyCsrfToken::class,
 ];
-?>
